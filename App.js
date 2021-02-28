167baf2f06c73ddc7d15f763464749d373839784
91aidenlee <91aiden.lee@gmail.com>
2021-02-28 23:58:14 +0900

    리액트 네이티브 플렉스 레이아웃 사용방법

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow",
+  },
+  blueView: {
+    flex: 3,
+    backgroundColor: "blue",
   },
 });
